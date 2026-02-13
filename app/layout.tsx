1ce05cbdecdae69459a9b37a67df5a0a93207a52
Josué Savy <josuesavy@josues-mbp.home>
2026-02-14 00:57:31 +0100

    chore: add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,25 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Josué Savy - Ingénieur Système',
-  description: 'Ingénieur diplômé de l\'ENIB, je suis passionné par l\'innovation. Spécialisé dans les technologies modernes, je crée des applications performantes et évolutives. Découvrez mes projets, mon parcours et mes compétences.',
-}
+  title: "Josué Savy - Ingénieur Système",
+  description:
+    "Ingénieur diplômé de l'ENIB, je suis passionné par l'innovation. Spécialisé dans les technologies modernes, je crée des applications performantes et évolutives. Découvrez mes projets, mon parcours et mes compétences.",
+  icons: {
+    icon: "/favicon.ico",
+  },
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="fr">
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
